6e6203e7c925c536605108cdd5a26ccf9683ab8b
Faiza Al Amoodi <faizaalamoodi@my.unt.edu>
2024-11-21 12:10:38 -0600

    Updated buttons functionalities.

--- Testing/popup.js
+++ Testing/popup.js
@@ -20,6 +20,11 @@ document.getElementById("scan-btn").addEventListener("click", async () => {
     const messages = await fetchGmailMessages(token);
     const scanResults = await analyzeMessages(messages);
     displayResults(scanResults);
+
+    const confPhish = document.getElementById("conf-btn");
+    confPhish.style.display = "inline-block";
+    confPhish.disabled = false;
+    document.getElementById("status").textContent = "Scanning complete! Ready to confirm.";
   } catch (error) {
     document.getElementById("status").textContent = "Failed to scan. Please try again later.";
     console.error("Error:", error);
