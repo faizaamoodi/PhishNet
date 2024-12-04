f9179f0f5163485e1a424a3ed43ca4493f75756c
Zoha <130319042+zoharaja@users.noreply.github.com>
2024-12-03 22:24:08 -0600

    Update script.js

--- Capstonetesting/frontend/script.js
+++ Capstonetesting/frontend/script.js
@@ -1,3 +1,3 @@
 document.getElementById('loginBtn').addEventListener('click', function() {
-    chrome.tabs.create({ url: 'register.html' });
+    chrome.tabs.create({ url: 'Capstonetesting/frontend/register.html' });
   });
