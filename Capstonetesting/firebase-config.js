cb82e8ac2f1a7d4ac8349bafc2dc60ddcc916449
Sowmya Reddy Tukakula <145711820+SowmyaTukakula@users.noreply.github.com>
2024-12-12 01:59:59 -0600

    firebase-config.js

---
+++ Capstonetesting/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGyK2CUBIsDen5PN6rR8wSV23JQXwcCes",
+  authDomain: "phishing-f4a58.firebaseapp.com",
+  projectId: "phishing-f4a58",
+  storageBucket: "phishing-f4a58.firebasestorage.app",
+  messagingSenderId: "789589561947",
+  appId: "1:789589561947:web:4bf5306862472c915c257e",
+  measurementId: "G-ST9ZKKDMXX"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
