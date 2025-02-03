dc4d57709ef548c225e842dc3c4ae80c1895024c
NifeTeye <151186986+NifeTeye@users.noreply.github.com>
2025-02-03 14:34:53 -0600

    Add files via upload

---
+++ Testing28/backend/phishing_detection.py
@@ -0,0 +1,17 @@
+from transformers import BertForSequenceClassification, BertTokenizer
+import torch
+
+model = BertForSequenceClassification.from_pretrained("./phishing_model")
+tokenizer = BertTokenizer.from_pretrained("./phishing_model")
+def predict_phishing(email_content):
+    inputs = tokenizer(email_content, return_tensors="pt", truncation=True, padding=True, max_length=512)
+    model.eval()
+    with torch.no_grad():
+        outputs = model(**inputs)
+        logits = outputs.logits
+        prediction = torch.argmax(logits, dim=1).item()
+    return "Phishing" if prediction == 1 else "Safe"
+
+#Example
+email = "Your account has been hacked, please verify your account here: https://suspiciouslink.com"
+print(predict_phishing(email))
